8a67a8ec0c5a5de118a4bdbb9eccd9dd1d98da7d
Alex Rosales <rosalesa512@gmail.com>
2022-04-19 18:52:59 -0700

    Added iterations for each question and prompted user in browser.

--- index.js
+++ index.js
@@ -2,4 +2,20 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
+let x = 0
+
 // Your Code Here
+for(  x = 0; x < weeklyExpenseQuestions.length; x++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[x]))
+    weeklyExpenses += answer
+}
+
+for ( x = 0; x < monthlyExpenseQuestions.length; x++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[x]))
+    monthlyExpenses += answer
+}
+
+for ( x = 0; x < annualExpenseQuestions.length; x++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[x]))
+    annualExpenses += answer
+}
